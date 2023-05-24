00aa4b110d05aac69796b91515f2e23577c8b4a5
nino <ninochkhikvadze7@gmail.com>
2023-05-24 14:53:57 +0400

    add js

---
+++ script.js
@@ -0,0 +1,23 @@
+// 1
+let index=100
+if (index<50)
+{console.log("less than 50")}
+else if(index>20)
+{console.log("more than 20")}
+else{
+    console.log("error")
+}
+// 2
+let userName="mariami"
+let result = ('userName =', !"mariami") ? 'True' : 'Error';
+console.log(result);
+
+// 3
+let script="mariam"
+switch(script){
+    case "mariam":
+    console.log("True") ;
+    break;
+    default:
+        console.log("False") ;
+}
